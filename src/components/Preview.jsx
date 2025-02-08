926591d9e1b87c7bb763ddbcebfe6ee99ebe4924
BikramGhart1 <coludingrain@gmail.com>
2025-02-08 17:56:04 +0545

    image shrink

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -43,14 +43,24 @@ const Preview = React.memo(() => {
           <textarea name="comment" id="comment" className='w-full border border-fuchsia-400 outline-none p-2'></textarea>
         </div>
         <div>
-          <div className='flex flex-row justify-start gap-x-8 pt-6'>
-            <div className='w-20 '>
+          <div className='flex flex-row justify-start gap-x-8 pt-10'>
+            <div className='min-w-20 flex-shrink-0'>
             <img src="https://c.disquscdn.com/uploads/users/39596/4769/avatar92.jpg?1688811803" className='rounded-full' alt="" />
             </div>
             <div className='flex flex-col'>
               <p className='font-semibold'>HariYadav</p> 
               <p className='text-sm'>2 years ago</p>
-              <p className='pt-2'>Tf u doin here old zaratul <br />Yeah, I'm doubting he's truly dead</p>
+              <p className='pt-2'>Tf u doin here old zaratul</p>
+            </div>
+          </div>
+          <div className='flex flex-row justify-start gap-x-8 pt-10'>
+            <div className='min-w-20 flex-shrink-0'>
+            <img src="https://c.disquscdn.com/uploads/users/35966/8923/avatar92.jpg?1723087776" className='rounded-full' alt="" />
+            </div>
+            <div className='flex flex-col'>
+              <p className='font-semibold'>Hans Bull</p> 
+              <p className='text-sm'>a year ago</p>
+              <p className='pt-2'>This is the 1st chapter, and I'm already confused with the comments</p>
             </div>
           </div>
         </div>
