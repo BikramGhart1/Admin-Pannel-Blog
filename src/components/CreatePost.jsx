7ad4b6433d9ee9bc10a2b19468dcdaa5ab643025
BikramGhart1 <coludingrain@gmail.com>
2025-02-18 19:29:42 +0545

    api simulation

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { useDispatch } from "react-redux";
-import { addPost,updatePost } from "../slices/postSlice";
+import { addPost, updatePost } from "../slices/postSlice";
 
 const CreatePost = React.memo(() => {
     const location = useLocation();
-    const dispatch=useDispatch();
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const [inputValue, setInputValue] = useState({
@@ -14,7 +14,7 @@ const CreatePost = React.memo(() => {
         title: "",
         date: "",
         blogContent: "",
-        // likes:0,
+        likes: 0,
     });
 
     useEffect(() => {
@@ -22,9 +22,9 @@ const CreatePost = React.memo(() => {
             setInputValue({
                 id: location.state.post.id || "",
                 title: location.state.post.title || "",
-                date:location.state.post.date||"",
+                date: location.state.post.date || "",
                 blogContent: location.state.post.blogContent || "",
-                // likes:location.state.post.likes||0,
+                likes: location.state.post.likes || 0,
             });
         }
     }, [location.state]);
@@ -34,17 +34,17 @@ const CreatePost = React.memo(() => {
     }
     const submithandler = (e) => {
         e.preventDefault();
-        const post={
-            id: inputValue.id||uuidv4(),
-            title:inputValue.title,
-            blogContent:inputValue.blogContent,
-            date: inputValue.date||new Date().toISOString(),
-            // likes:inputValue.likes,
-            
+        const post = {
+            id: inputValue.id || uuidv4(),
+            title: inputValue.title,
+            blogContent: inputValue.blogContent,
+            date: inputValue.date || new Date().toISOString(),
+            likes: inputValue.likes,
+
         }
-        if(inputValue.id){
+        if (inputValue.id) {
             dispatch(updatePost(post));
-        }else{
+        } else {
             dispatch(addPost(post));
         }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,13 +4,17 @@ import { calcuateLikes } from '../slices/postSlice';
 
 export default function Profile() {
     const [isEditMode, setIsEditMode] = useState(false)
-    const [inputValue,setInputValue]=useState({
-        userName:'Username',
-        bio:'Bio'
+    const [inputValue, setInputValue] = useState({
+        userName: 'Username',
+        bio: 'Bio'
     });
     const posts = useSelector((states) => states.posts.posts);
     const totalLikes = useSelector((state) => state.posts.totalLikes)
     const dispatch = useDispatch();
+    const [userData, setUserData] = useState({
+        userName: "Username",
+        bio: "Bio"
+    })
     // const totalLikes = dispatch(calcuateLikes())
     useEffect(() => {
         dispatch(calcuateLikes());
@@ -19,15 +23,50 @@ export default function Profile() {
     const editHandler = () => {
         setIsEditMode(!isEditMode);
     }
-    const onChangeHandler=(e)=>{
-       const {name,value}=e.target;
-       setInputValue((prev)=>({...prev,[name]:value}))
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setInputValue((prev) => ({ ...prev, [name]: value }))
 
     }
-    const saveForm=(e)=>{
-       e.preventDefault();
-       setIsEditMode(false);
+    const saveForm = async (e) => {
+        e.preventDefault();
+        setIsEditMode(false);
+
+        //These are simulation of fetching data from node js application 
+        try {
+            const response = await fetch("http://localhost:3000/addName", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    userName: inputValue.userName,
+                    bio: inputValue.bio
+                }),
+            });
+            if (!response.ok) {
+                throw new Error("Failed to create new user")
+            }
+            const data = await response.json();
+        } catch (err) {
+            console.error("Error adding user: ", err);
+        }
+        try {
+            const response = await fetch("http://localhost:3000/", {
+                method: "GET",
+            });
+            if (!response.ok) {
+                throw new Error("Failed getting the data from API");
+            }
+            const data = await response.json();
+            setUserData(data);
+            console.log(data);
+        } catch (err) {
+            console.error("An error occurred: ", err);
+        }
+
     }
+
     return (
         <div className='p-5'>
             <form className='flex flex-row justify-start items-start gap-x-7 md:gap-x-14 w-full' onSubmit={saveForm}>
@@ -45,14 +84,14 @@ export default function Profile() {
                         {isEditMode ?
                             (<input className='border border-neutral-500 p-1 rounded-md outline-none' name='userName' value={inputValue.userName} onChange={onChangeHandler} type='text' placeholder='Username' />)
                             :
-                            (<p className='font-semibold'>{inputValue.userName}</p>)
+                            (<p className='font-semibold'>{userData.userName}</p>)
                         }
                         {
                             isEditMode ? (
                                 <textarea name='bio' value={inputValue.bio} className='outline-none border border-neutral-500 p-1 rounded-md min-h-10' onChange={onChangeHandler}></textarea>
                             ) : (
 
-                                <p>{inputValue.bio}</p>
+                                <p>{userData.bio}</p>
                             )
                         }
                     </div>
@@ -68,6 +107,10 @@ export default function Profile() {
                 }
 
             </form>
+            <div className='mt-5 p-3 bg-yellow-200'>
+                <p>This will not work now cause I am simulating the fetch api from node js server.</p>
+                <p>Will integrate with the backend in later on.</p>
+            </div>
         </div>
     )
 }
